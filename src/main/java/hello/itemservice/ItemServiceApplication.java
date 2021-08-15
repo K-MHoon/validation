ca8b0e268745ec0d9ad2eb69544c6c55a7b39e1a
K-MHoon <azmria117@gmail.com>
2021-08-15 23:39:11 +0900

    08.14 ~ 08.15 Validation

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,7 +1,9 @@
 package hello.itemservice;
 
+import hello.itemservice.web.validation.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class ItemServiceApplication {
